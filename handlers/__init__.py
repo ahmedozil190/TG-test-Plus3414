14ebf1a6a7698837d22c3127df490e58cf3bb7b8
ahmedozil190 <storykids131@gmail.com>
2026-04-24 18:34:58 +0300

    Clean up unused python code and telegram bot handlers

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,13 +1,7 @@
 from .start import router as start_router
-from .wallet import router as wallet_router
-from .sell import router as sell_router
-from .buy import router as buy_router
 from .admin import router as admin_router
 from aiogram import Router
 
 main_router = Router()
 main_router.include_router(admin_router)
 main_router.include_router(start_router)
-main_router.include_router(wallet_router)
-main_router.include_router(sell_router)
-main_router.include_router(buy_router)
--- keyboards/client.py
+++ keyboards/client.py
@@ -8,17 +8,3 @@ def main_keyboard():
         ]
     ])
 
-def profile_keyboard():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="➕ شحن الرصيد", callback_data="deposit")],
-        [InlineKeyboardButton(text="الرجوع 🔙", callback_data="back_main")]
-    ])
-
-def sell_menu_keyboard():
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="- Selling an account.", url="https://t.me/MOOO8O")],
-        [InlineKeyboardButton(text="Account prices.", callback_data="sell_prices")],
-        [InlineKeyboardButton(text="- Pull my balance.", callback_data="pull_balance")],
-        [InlineKeyboardButton(text="Prices channel", url="https://t.me/MOOO8O")],
-        [InlineKeyboardButton(text="- Return.", callback_data="back_main")]
-    ])
